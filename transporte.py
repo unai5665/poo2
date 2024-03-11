342aae36221e9840920240d79e96c64903d4276d
unai5665 <unaiperez5665@gmail.com>
2024-03-11 12:51:17 +0100

    añdido transporte

---
+++ transporte.py
@@ -0,0 +1,14 @@
+class Transporte:
+    def __init__(self, *, ruedas:int, asientos:int) -> None:
+        self.ruedas = ruedas
+        self.asientos = asientos
+
+def desplazar(x:int, y:int = 0) -> None:
+    print("Desplazando a: ", x, y)
+
+
+def informacion(self) -> None:
+    ("Ruedas: ")
+
+
+
